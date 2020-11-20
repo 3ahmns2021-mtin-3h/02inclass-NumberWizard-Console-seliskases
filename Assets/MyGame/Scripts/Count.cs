4433a0253b615464ba603c73a72b887b02222848
simoneliskases <simon.eliskases@outlook.de>
2020-11-20 12:50:27 +0100

    Added Game Mode Count

--- Assets/MyGame/Scripts/Count.cs
+++ Assets/MyGame/Scripts/Count.cs
@@ -5,32 +5,68 @@ using TMPro;
 
 public class Count : MonoBehaviour
 {
-    public TMP_InputField inputGuess, inputMin, inputMax;
+    public TMP_InputField inputNumber, inputMin, inputMax;
     public TextMeshProUGUI textField;
-    
-    private float guess, min, max, range;
+
+    private float number, min, max, range, maxCount;
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Return))
         {
-            guess = float.Parse(inputGuess.text);
+            number = float.Parse(inputNumber.text);
             min = float.Parse(inputMin.text);
             max = float.Parse(inputMax.text);
             range = max - min;
 
-            WriteCount();
+            if(number>=min && number <= max)
+            {
+                WriteCount();
+            }
+            else
+            {
+                Debug.LogWarning("Your number is not within your given Range");
+            }
+
         }
     }
 
     private void WriteCount()
     {
-        textField.text = CalculateCount().ToString();
+        textField.text = CalculateCount(min, max).ToString();
     }
 
-    private float CalculateCount()
+    private int CalculateCount(float tempMin, float tempMax)
+    {
+        float guess;
+        int count=0;
+
+        for(int n = 1; n <= CalculateMax(); n++)
+        {
+            count = n;
+            guess = Mathf.FloorToInt((tempMin + tempMax) / 2);
+
+            if(guess > number)
+            {
+                tempMax = guess;
+            }
+
+            if(guess < number)
+            {
+                tempMin = guess;
+            }
+
+            if(guess == number)
+            {
+                break;
+            }
+        }
+        return(count);
+    }
+
+    private float CalculateMax()
     {
         float maxCount = Mathf.Ceil(Mathf.Log(range, 2));
-        return maxCount;
+        return(maxCount);
     }
 }
